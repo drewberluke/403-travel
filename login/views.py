5213d2f4157b59bb1b9dd81b3c6b6e40bd4dfd29
Andrew Luke <aluke4@byu.edu>
2020-11-04 14:12:38 -0700

    added templates folder and functions

--- login/views.py
+++ login/views.py
@@ -3,5 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def indexPageView(request):
-    return HttpResponse('Yo whatupppp')
-    
+    return render(request, 'login/index.html')
